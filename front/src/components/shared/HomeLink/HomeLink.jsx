1ff9ef214f666f3b0c3518b8c93dfc68e420a915
Vlad Shulga <34230876+VladShg@users.noreply.github.com>
2022-04-22 22:42:23 +0300

    refactor link to use NavLink

--- front/src/components/shared/HomeLink/HomeLink.jsx
+++ front/src/components/shared/HomeLink/HomeLink.jsx
@@ -1,6 +1,6 @@
 import classNames from "classnames"
 import React from "react"
-import { Link, useLocation } from "react-router-dom"
+import { NavLink } from "react-router-dom"
 import { HomeStyles as styles } from "../../../styles/_Styles"
 import PropTypes from "prop-types"
 
@@ -12,12 +12,15 @@ HomeLink.propTypes = {
 }
 
 export default function HomeLink(props) {
-	let localProps = { ...props }
-	let location = useLocation()
-	if (localProps.to && location.pathname === localProps.to) {
-		let className = localProps.className || ""
-		className = classNames(className, styles.active)
-		localProps.className = className
+	let { className, ...localProps } = { ...props }
+	const getStyles = ({ isActive }) => {
+		let regular = className || ""
+		if (isActive) return classNames(regular, styles.active)
+		return regular
 	}
-	return <Link {...localProps}>{props.children}</Link>
+	return (
+		<NavLink {...localProps} className={getStyles}>
+			{props.children}
+		</NavLink>
+	)
 }
