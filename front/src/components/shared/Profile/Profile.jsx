be250d09fdfe0c0010f7bad2ed2267621c6a2b7e
Vlad Shulga <34230876+VladShg@users.noreply.github.com>
2022-05-07 14:12:54 +0300

    Profile logout dropdown

--- front/src/components/shared/Profile/Profile.jsx
+++ front/src/components/shared/Profile/Profile.jsx
@@ -1,25 +1,67 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { authSelector, logout } from '../../../store/auth/authSlice'
 import styles from './Profile.module.scss'
 import Avatar from '../Avatar/Avatar'
+import classNames from 'classnames'
 
 export default function Profile() {
 	const { user } = useSelector(authSelector)
 	const { name, username, avatar } = user || {}
+	const [menuOpen, setMenuOpen] = useState(false)
 	const dispatch = useDispatch()
 
 	if (!user) {
 		return null
 	}
 
-	return (
-		<div className={styles.container} onClick={() => dispatch(logout())}>
-			<Avatar src={avatar} />
-			<div className={styles.description}>
-				<span>{name}</span>
-				<span className={styles.username}>{'@' + username}</span>
+	function Container({ children, className, ...props }) {
+		return (
+			<div
+				className={classNames(styles.Container, { [className]: !!className })}
+				{...props}
+			>
+				{children}
 			</div>
+		)
+	}
+
+	function Descryption({ children }) {
+		return <div className={styles.Description}>{children}</div>
+	}
+
+	function Name({ name }) {
+		return <span>{name}</span>
+	}
+
+	function Username({ username }) {
+		return <span className={styles.Username}>{'@' + username}</span>
+	}
+
+	return (
+		<div className={styles.Wrapper}>
+			<div className={classNames(styles.DropDown, { [styles.Open]: menuOpen })}>
+				<Container className={styles.InnerContainer}>
+					<Avatar src={avatar} />
+					<Descryption>
+						<Name name={name} />
+						<Username username={username} />
+					</Descryption>
+				</Container>
+				<button onClick={() => dispatch(logout())}>
+					Log out from @{username}
+				</button>
+			</div>
+			<Container onClick={() => setMenuOpen(!menuOpen)}>
+				<Avatar src={avatar} />
+				<Descryption>
+					<div className={styles.Icon}>
+						<i className="fa-solid fa-ellipsis"></i>
+					</div>
+					<Name name={name} />
+					<Username username={username} />
+				</Descryption>
+			</Container>
 		</div>
 	)
 }
