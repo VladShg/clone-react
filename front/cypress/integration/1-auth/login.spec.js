2aef699b015fe5bed56ba274b75691a2623d5f23
Vled <lubnyvs@gmail.com>
2022-05-26 20:59:27 +0300

    log in test

--- front/cypress/integration/1-auth/login.spec.js
+++ front/cypress/integration/1-auth/login.spec.js
@@ -3,7 +3,9 @@ describe('Login page', () => {
 	const name = 'Vlad Shulha'
 	const birth = '2000-07-30'
 	const email = 'email@mail.com'
-	it('Sign up and log out', () => {
+	const password = 'password'
+
+	it('Sign up, log out (credentials)', () => {
 		cy.visit('/')
 		cy.contains('Sign up with email').click()
 		cy.get('input[name="name"]').type(name)
@@ -19,9 +21,26 @@ describe('Login page', () => {
 		cy.contains('Skip').should('not.be', 'disabled')
 		cy.contains('Skip').click()
 
-		cy.get('input[placeholder=Password]').type('password')
-		cy.get('input[placeholder="Confirm password"]').type('password')
+		cy.get('input[placeholder=Password]').type(password)
+		cy.get('input[placeholder="Confirm password"]').type(password)
 		cy.contains('Submit').should('not.be', 'disabled')
+		cy.contains('Submit').click()
+
+		cy.contains('@' + username).should('exist')
+		cy.contains(name).should('exist')
+
+		cy.get('span[class*="Username"]:visible').should('contain', '@' + username)
+		cy.get('span[class*="Username"]:visible').click()
+		cy.contains('Log out').click()
+
+		cy.contains('Sign up with email').should('exist')
+	})
+
+	it('Log in (credentials)', () => {
+		cy.contains('Sign in').click()
+		cy.get('input[placeholder="Username or login"]').type(username)
+		cy.contains('Continue').click()
+		cy.get('input[placeholder="Password"]').type(password)
 		cy.contains('Submit').click()
 
 		cy.contains('@' + username).should('exist')
