4f7fd0ceca011f56302bc77165971ff0e2a7136b
Vlad Shulga <34230876+VladShg@users.noreply.github.com>
2022-06-02 22:52:57 +0300

    Separate themes

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,51 +1,11 @@
-import { ThemeProvider, createTheme } from '@mui/material/styles'
+import { ThemeProvider } from '@mui/material/styles'
+import { theme } from '@utils/theme'
 import React, { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import Router from './routes/Router'
 import { useLazyAuthorizeQuery } from './services/authApi'
 import { useLazyGetAvatarQuery } from './services/userApi'
 import { authSelector, setToken, setUser } from './store/auth/authSlice'
-import { common } from '@mui/material/colors'
-import { alpha } from '@mui/material'
-
-const theme = createTheme({
-	typography: {
-		fontFamily: 'Manrope',
-		h1: { fontSize: '2em', margin: '0.67em 0', fontWeight: 'bold' },
-		h2: { fontSize: '1.5em', margin: '0.83em 0', fontWeight: 'bold' },
-		h3: { fontSize: '1.17em', margin: '1em 0', fontWeight: 'bold' },
-		notice: { fontSize: '10px', lineHeight: '12px' },
-		modalSub: {
-			color: common.black,
-			fontWeight: '700',
-			fontSize: '15px',
-			lineHeight: '20px',
-		},
-		modalTitle: {
-			color: common.black,
-			fontWeight: '700',
-			fontSize: '31px',
-			lineHeight: '36px',
-		},
-		modalDesc: {
-			color: '#536471',
-			fontWeight: '400',
-			fontSize: '14px',
-			lineHeight: '16px',
-		},
-	},
-	palette: {
-		primary: {
-			main: '#1d9bf0',
-			dark: '#1a8cd8',
-			inavtive: alpha('#1d9bf0', 0.4),
-		},
-		common: {
-			dark: '#536471',
-		},
-		tonalOffset: 0.2,
-	},
-})
 
 export default function App() {
 	const { token, user } = useSelector(authSelector)
---
+++ front/src/utils/theme.js
@@ -0,0 +1,41 @@
+import { common } from '@mui/material/colors'
+import { alpha, createTheme } from '@mui/material'
+
+export const theme = createTheme({
+	typography: {
+		fontFamily: 'Manrope',
+		h1: { fontSize: '2em', margin: '0.67em 0', fontWeight: 'bold' },
+		h2: { fontSize: '1.5em', margin: '0.83em 0', fontWeight: 'bold' },
+		h3: { fontSize: '1.17em', margin: '1em 0', fontWeight: 'bold' },
+		notice: { fontSize: '10px', lineHeight: '12px' },
+		modalSub: {
+			color: common.black,
+			fontWeight: '700',
+			fontSize: '15px',
+			lineHeight: '20px',
+		},
+		modalTitle: {
+			color: common.black,
+			fontWeight: '700',
+			fontSize: '31px',
+			lineHeight: '36px',
+		},
+		modalDesc: {
+			color: '#536471',
+			fontWeight: '400',
+			fontSize: '14px',
+			lineHeight: '16px',
+		},
+	},
+	palette: {
+		primary: {
+			main: '#1d9bf0',
+			dark: '#1a8cd8',
+			inavtive: alpha('#1d9bf0', 0.4),
+		},
+		common: {
+			dark: '#536471',
+		},
+		tonalOffset: 0.2,
+	},
+})
